22d1cf19b12a6f838cc851985c1d2f0ecffb07f4
”Sammy <sammyolsson@gmail.com>
2023-11-27 20:53:37 +0100

    Created Routes, Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { NewNote } from "./NewNote";
 
 function App() {
-  return <h1>Hi</h1>
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />
+          <Route path="edit" element={<h1>Edit</h1>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  );
 }
 
-export default App
+export default App;
